23a9c8ee96ebcd250b03c84cd590c785bd325b4b
Unknown <roxxanrebirth@gmail.com>
2019-06-22 01:28:12 -0400

    Updated Loot_Table
    Removed Deprecated Method now using JSON Loot_Tables for blocks

--- src/main/java/sora/dwarfcoal/blocks/BlockBase.java
+++ src/main/java/sora/dwarfcoal/blocks/BlockBase.java
@@ -1,15 +1,8 @@
 package sora.dwarfcoal.blocks;
 
-import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootContext;
 import sora.dwarfcoal.DwarfCoal;
-
-import java.util.List;
 
 public class BlockBase extends Block {
   public BlockBase(String name, Properties p_i48440_1_) {
@@ -17,8 +10,8 @@ public class BlockBase extends Block {
     this.setRegistryName(new ResourceLocation(DwarfCoal.MODID,name));
   }
 
-  @Override
+ /* @Override
   public List<ItemStack> getDrops(BlockState p_220076_1_, LootContext.Builder p_220076_2_) {
     return Lists.newArrayList(new ItemStack(Item.getItemFromBlock(this)));
-  }
+  }*/
 }
